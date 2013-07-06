27c0e0ee4616c6dfee4fe6757880096cafceb0c5
John Smith <example@stanford.edu>
2013-07-06 16:14:45 +0000

    fix code

--- web.js
+++ web.js
@@ -5,9 +5,11 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var content = fs.readFileSync('index.html');
-  var buffer = new Buffer(content);
+  var buffer = new Buffer(content, "utf-8");
 
-  response.send(buffer);
+  response.writeHead(200);
+  response.write(buffer.toString('utf-8'));
+  response.end();
 });
 
 var port = process.env.PORT || 5000;
